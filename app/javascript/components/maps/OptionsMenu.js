dc02982d31dc3b20fb1e174735327290f837adae
Edwin Jimenez <ejjimenezc@unal.edu.co>
2018-10-14 14:31:43 -0500

    Mejorado componente de menu

--- app/javascript/components/maps/OptionsMenu.js
+++ app/javascript/components/maps/OptionsMenu.js
@@ -1,19 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const element = (
-      <div>
-        <h1>¿Que Buscas?</h1>
-        <li>Planear Ruta</li>
-        <li>Sitios de interes</li>
-        <li>Ultimos acontecimientos</li>
+function Menu() {
+  return (
+      <div id="menu">
+        <h1 ><font size="18">¿Que Buscas?</font></h1>
+        <div>
+          <li>Planear Ruta</li>
+          <li>Sitios de interes</li>
+          <li>Ultimos acontecimientos</li>
+        </div>
       </div>
-)
+    )
+}
+
+const main = <Menu />
 
 class OptionsMenu extends React.Component {
   render () {
     return (
-      element
+      main
     );
   }
 }
