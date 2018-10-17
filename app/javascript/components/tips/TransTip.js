85646ac8a361c17a0709e53368b558f4d20168b1
ejjimenezc <ejjimenezc@unal.edu.co>
2018-10-17 17:54:51 -0500

    React Componentes para la pagina de Tips.

---
+++ app/javascript/components/tips/TransTip.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class TransTip extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TransTip
