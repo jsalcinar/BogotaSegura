aef9fdc346f8b8e1acb9e6bc32dd51a78d140b1b
Edwin Jimenez <ejjimenezc@unal.edu.co>
2018-10-20 08:55:29 -0500

    Mejorado estilo de la ventana de error.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -9,12 +9,80 @@ module DeviseHelper
                       resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
+    <style type="text/css">
+      /* The Modal (background) */
+      .modal {
+          display: none; /* Hidden by default */
+          position: fixed; /* Stay in place */
+          z-index: 1; /* Sit on top */
+          padding-top: 100px; /* Location of the box */
+          left: 0;
+          top: 0;
+          padding-left: 20%;
+          padding-right: 20%;
+          width: 100%; /* Full width */
+          height: 100%; /* Full height */
+          overflow: auto; /* Enable scroll if needed */
+          background-color: rgb(0,0,0); /* Fallback color */
+          background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+      }
+
+      /* Modal Content */
+      .modal-content {
+          position: relative;
+          background-color: #fefefe;
+          margin: auto;
+          padding: 0;
+          border: 1px solid #888;
+          width: 80%;
+          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+          -webkit-animation-name: animatetop;
+          -webkit-animation-duration: 0.4s;
+          animation-name: animatetop;
+          animation-duration: 0.4s
+      }
+
+      /* Add Animation */
+      @-webkit-keyframes animatetop {
+          from {top:-300px; opacity:0} 
+          to {top:0; opacity:1}
+      }
+
+      @keyframes animatetop {
+          from {top:-300px; opacity:0}
+          to {top:0; opacity:1}
+      }
+
+      /* The Close Button */
+      .close {
+          color: white;
+          float: right;
+          font-size: 28px;
+          font-weight: bold;
+      }
+
+      .close:hover,
+      .close:focus {
+          color: #000;
+          text-decoration: none;
+          cursor: pointer;
+      }
+
+      .modal-header {
+          padding: 2px 16px;
+          background-color: black;
+          color: white;
+          align-items: center;
+      }
+
+      .modal-body {padding: 2px 16px;}
+    </style>
+
     <div id="myModal" class="modal">
 
       <!-- Modal content -->
       <div class="modal-content">
         <div class="modal-header">
-          <h2>Error</h2>
           <span class="close">&times;</span>
         </div>
         <div class="modal-body">
