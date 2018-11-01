3f9e18a5e36b99bee6974c5f59e418f32bb81704
Edwin Javier Jimenez Casares <trickyjuggler96@gmail.com>
2018-11-01 15:42:47 +0000

    Correccion semilla de usuarios

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,7 +1,7 @@
 users_list = [
-  [ "administrator", "administrator@gmail.com", "123456", "image/upload/v1541084740/bogotasegura/avatar/default/administrator.png", true ]
-  [ "administrator", "administrator@gmail.com", "bogota", "image/upload/v1541084537/bogotasegura/avatar/default/hector.png", false ]
-  [ "hector", "hector@gmail.com", "bogota", "image/upload/v1541084536/bogotasegura/avatar/default/default.png", false ]
+  [ "administrator", "administrator@gmail.com", "123456", "image/upload/v1541084740/bogotasegura/avatar/default/administrator.png", true ],
+  [ "administrator", "administrator@gmail.com", "bogota", "image/upload/v1541084537/bogotasegura/avatar/default/hector.png", false ],
+  [ "hector", "hector@gmail.com", "bogota", "image/upload/v1541084536/bogotasegura/avatar/default/default.png", false ],
   [ "ariel", "ariel@gmail.com", "bogota", "image/upload/v1541084536/bogotasegura/avatar/default/default.png", false ],
   [ "adam", "adam@gmail.com", "bogota", "image/upload/v1541084536/bogotasegura/avatar/default/default.png", false ],
   [ "george", "george@gmail.com", "bogota", "image/upload/v1541084536/bogotasegura/avatar/default/default.png", false ],
@@ -10,5 +10,5 @@ users_list = [
 ]
 
 users_list.each do |username, email, password, avatar, admin|
-  User.create( username: username, email: email, password: password, avatar: avatar: avatar, admin: admin )
+  User.create( username: username, email: email, password: password, avatar: avatar, admin: admin )
 end
