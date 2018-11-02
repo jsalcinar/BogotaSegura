977c17afde3bbb48a3a591cfbbd9f1b59bc59ed4
Edwin Javier Jimenez Casares <trickyjuggler96@gmail.com>
2018-11-02 18:18:11 +0000

    Mejorada la carga de imagenes en editar perfil

--- app/assets/javascripts/devise/edit.js
+++ app/assets/javascripts/devise/edit.js
@@ -1,30 +1,5 @@
 function edit_init(){
-    edit_pic();
-    change_preview();
-}
-
-function edit_pic(){
-    $( "#user_avatar" ).click(function() {
-        $( "#avatar-upload" ).trigger( "click" );
-    });
-}
-
-function change_preview(){
-    $(function() {
-      function readURL(input) {
-        if (input.files && input.files[0]) {
-          var reader = new FileReader();
-    
-          reader.onload = function (e) {
-            $('#user_avatar').attr('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-        }
-      }
-    
-      $("#avatar-upload").change(function(){
-        $('#user_avatar').removeClass('hidden');
-        readURL(this);
-      });
-    });
+    pic_preview();
+    pic_as_button();
+    detect_resize_window();
 }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_admin, :except => [:show]
 
   def index
+    @users = User.all
   end
 
   def show
