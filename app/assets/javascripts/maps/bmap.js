c40b249b4a83fae697aa360016f39db91f2a7b48
Edwin Javier Jimenez Casares <trickyjuggler96@gmail.com>
2019-02-21 20:46:38 +0000

    Añadido menu de control

--- app/assets/javascripts/maps/bmap.js
+++ app/assets/javascripts/maps/bmap.js
@@ -1,4 +1,5 @@
 var map;
+
 var mapCenter = {lat: 4.6371933, lng: -74.0826976};
 
 function getMap(){
@@ -7,5 +8,17 @@ function getMap(){
         center: new Microsoft.Maps.Location(mapCenter.lat, mapCenter.lng),
         mapTypeId: Microsoft.Maps.MapTypeId.road,
         zoom: 16
+    });
+    
+    //Load the directions module.
+    Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
+        //Create an instance of the directions manager.
+        directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
+
+        //Specify where to display the route instructions.
+        directionsManager.setRenderOptions({ itineraryContainer: '#directionsItinerary' });
+
+        //Specify the where to display the input panel
+        directionsManager.showInputPanel('directionsPanel');
     });
 }
